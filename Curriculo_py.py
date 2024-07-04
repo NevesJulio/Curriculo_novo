bec478ef445dcf96c3d2c9cc49c63237772f7b04
NevesJulio <juliocesar-nascimento@hotmail.com>
2024-07-04 16:12:18 -0300

    criei uma função pra automatizar a exibição das informações no pdf

--- Curriculo_py.py
+++ Curriculo_py.py
@@ -6,15 +6,15 @@ from fpdf import FPDF
 cnv = canvas.Canvas("MeuCurrículo.pdf")
 largura_A4, altura_A4 = A4
 
-content = [
-    ["Formação Acadêmica","Bacharelado em Engenharia Biomédica - UFRN - Previsão de Conclusão: 2026","Bacharelado em Ciência e Tecnologia - UFRN - Conclusão: 2021"],
-    ["Experiência","Monitor - UFRN - [Período das Monitorias]","Auxílio aos alunos em suas dúvidas e \n dificuldades nas disciplinas.","Preparação de materiais didáticos e suporte ao professor na correção de exercícios e provas.","Contribuição para o desenvolvimento de projetos acadêmicos relacionados às disciplinas."],
-    ["Habilidades Técnicas","Python, C++, C#, HTML, CSS","Ferramentas de Desenvolvimento: [IDE's, plataformas de desenvolvimento, etc.]","Bancos de Dados: [SQL, NoSQL, etc.]"]
+content1 = [
+    ["Formação Acadêmica","Bacharelado em Engenharia Biomédica \n - UFRN - Previsão de Conclusão: 2026","Bacharelado em Ciência e Tecnologia \n - UFRN - Conclusão: 2021"],
+    ["Experiência","Monitor - UFRN - [Período das Monitorias]","Auxílio aos alunos em suas dúvidas e \n dificuldades nas disciplinas.","Preparação de materiais didáticos \n e suporte ao professor na correção de \n exercícios e provas.","Contribuição para o desenvolvimento de \n projetos acadêmicos relacionados às \n disciplinas."],
+    ["Habilidades Técnicas","Python, C++, C#, HTML, CSS","Ferramentas de Desenvolvimento: \n [IDE's, plataformas de desenvolvimento, etc.]","Bancos de Dados: [SQL, NoSQL, etc.]"]
 ]
 
-print(len(content))
+
 posicao_ret = 250
-posicao = [0,altura_A4-200]
+posicao1 = [0,altura_A4-100]
 
 
 cnv.setFillColorRGB(0.2, 0.5, 0.8)
@@ -22,29 +22,54 @@ cnv.rect(0,0,posicao_ret,altura_A4, fill=True)
 
 
 
-cnv.setFont("Helvetica", 5)
 
-for j in range(len(content)):
-    posicao[1] = posicao[1] - 50
-    for i in range(len(content[j])):
-        posicao[1] = posicao[1]-25
-        cnv.setFillColorRGB(0, 0, 0)
-        if i == 0:
-            cnv.setFont("Helvetica-Bold", 12)
-            cnv.drawString(10, posicao[1], content[j][i])
-        else:
-            cnv.setFont("Helvetica", 12)
-            cnv.drawString(20, posicao[1], content[j][i])
-            cnv.setFillColorRGB(0, 0, 0)
-            cnv.circle(10, posicao[1]+5, 2, fill=True)
 
+#for j in range(len(content)):
+    
+#   for i in range(len(content[j])):
+#       posicao[1] = posicao[1]-25
+#       cnv.setFillColorRGB(0, 0, 0)
+#       if i == 0:
+#           cnv.setFont("Helvetica-Bold", 12)
+#           cnv.drawString(10, posicao[1], content[j][i])
+#       else:
+#           cnv.setFillColorRGB(0, 0, 0)
+#           cnv.circle(10, posicao[1]+5, 2, fill=True)
+#           content[j][i] = content[j][i].split("\n")
+#           print(len(content[j][i]))
+#           for k in range(len(content[j][i])):
+#               cnv.setFillColorRGB(1, 1, 1)
+#               cnv.setFont("Helvetica", 11)
+#               cnv.drawString(20, posicao[1], content[j][i][k])
+#               posicao[1] = posicao[1] - 15
+#   posicao[1] = posicao[1] - 50
         
-       
+
+def Exibir(content, posicao):
+    
+    for j in range(len(content)):
+        
+        for i in range(len(content[j])):
+            posicao[1] = posicao[1]-25
+            cnv.setFillColorRGB(0, 0, 0)
+            if i == 0:
+                cnv.setFont("Helvetica-Bold", 12)
+                cnv.drawString(10, posicao[1], content[j][i])
+            else:
+                cnv.setFillColorRGB(0, 0, 0)
+                cnv.circle(10, posicao[1]+5, 2, fill=True)
+                content[j][i] = content[j][i].split("\n")
+                print(len(content[j][i]))
+                for k in range(len(content[j][i])):
+                    cnv.setFillColorRGB(1, 1, 1)
+                    cnv.setFont("Helvetica", 11)
+                    cnv.drawString(20, posicao[1], content[j][i][k])
+                    posicao[1] = posicao[1] - 15
+        posicao[1] = posicao[1] - 50
 
 
 
-
-
+Exibir(content1, posicao1)
 
 
 class Section:
