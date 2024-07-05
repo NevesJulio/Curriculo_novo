1fb5a18cd4871cfe1334fa6ff114924c10c54e74
NevesJulio <juliocesar-nascimento@hotmail.com>
2024-07-05 01:46:12 -0300

    Coloquei a imagem, defini as posições e mudei a cor, além de adicionar algumas decorações

--- Curriculo_py.py
+++ Curriculo_py.py
@@ -17,60 +17,50 @@ contact_info = [[
 
 
 content1 = [
-    ["Formação Acadêmica","Bacharelado em Engenharia Biomédica\n- UFRN - Previsão de Conclusão: 2026","Bacharelado em Ciência e Tecnologia\n- UFRN - Conclusão: 2021", "Curso de python - Udemy - Conclusão: 2022", "Curso de API - Udemy - Conclusão: 2022"],
-    ["Experiência","Monitoria - UFRN -\nCálculo III - 2022.2\nFísica Experimental II - 2023.1/2023.2\nEletricidade Aplicada - 2024.1","Auxílio aos alunos em suas dúvidas e\ndificuldades nas disciplinas.","Preparação de materiais didáticos\ne suporte ao professor na correção de\nexercícios e provas.","Iniciação Científica - Instituto Santos Drumond -\nAuxílio no desenvolvimento de projeto de\npesquisa voltados para a área de robótica e\nprogramção"],
-    ["Habilidades Técnicas","Python, C++, C#, HTML, CSS","Ferramentas de Desenvolvimento:\n[IDE's, plataformas de desenvolvimento, etc.]","Inglês: Fluente"]
+    ["Formação Acadêmica","Engenharia Biomédica - UFRN -\n 7º período (Previsão de Conclusão: 2025)","Bacharelado em Ciência e Tecnologia\n- UFRN - Conclusão: 2021", "Curso de python - Udemy - Conclusão: 2022", "Curso de API - Udemy - Conclusão: 2022"]
 ]
 content2 = [
-    ["Áreas de interesse", "Tenho o objetivo de me aprofundar mais em conhecimentos\npráticos nas áreas de Eletrônica, assim como em programação,\nprincipalmente em python e C++"],
-    [" "],
-    [" "]
+    ["Áreas de interesse", "Tenho o objetivo de me aprofundar mais\nem conhecimentos práticos nas áreas de\nEletrônica, assim como em programação,\nprincipalmente em python e C++."]
+    
 ]
+content3 = [
+    ["Experiência","Monitoria - UFRN -\nCálculo III - 2022.2\nFísica Experimental II - 2023.1/2023.2\nEletricidade Aplicada - 2024.1","Auxílio aos alunos em suas dúvidas e\ndificuldades nas disciplinas.","Preparação de materiais didáticos\ne suporte ao professor na correção de\nexercícios e provas.","Iniciação Científica - Instituto Santos Drumond -\nAuxílio no desenvolvimento de projeto de\npesquisa voltados para a área de robótica e\nprogramção"],
+    ["Habilidades Técnicas","Python, C++, C#, HTML, CSS","Ferramentas de Desenvolvimento:\n[IDE's, plataformas de desenvolvimento, etc.]"],
+    ["Idiomas","Inglês Fluente"]
+]
+
 
 
 posicao_ret = 250
 posicao0 = [20,altura_A4-20]
-posicao1 = [20,altura_A4-100]
+posicao1 = [20,altura_A4-220]
 posicao3 = [260, altura_A4-50]
+posicao4 = [40,altura_A4-150]
+posicao4_copia = [280, altura_A4-140]
 
 
-cnv.setFillColorRGB(0.2, 0.5, 0.8)
+
+
+#Cor do fundo
+cnv.setFillColorRGB(0.9, 0.9, 0.9)
+cnv.rect(0,0,largura_A4,altura_A4, fill=True, stroke = 0)
+
+#Cor do retangulo
+cnv.setFillColorRGB(115/255, 69/255, 85/255)
 cnv.setStrokeColorRGB(0.2, 0.5, 0.8)
-cnv.rect(0,0,posicao_ret,altura_A4, fill=True)
+cnv.rect(0,0,posicao_ret,altura_A4, fill=True, stroke = 0)
 
 
 
 
-
-#for j in range(len(content)):
-    
-#   for i in range(len(content[j])):
-#       posicao[1] = posicao[1]-25
-#       cnv.setFillColorRGB(0, 0, 0)
-#       if i == 0:
-#           cnv.setFont("Helvetica-Bold", 12)
-#           cnv.drawString(10, posicao[1], content[j][i])
-#       else:
-#           cnv.setFillColorRGB(0, 0, 0)
-#           cnv.circle(10, posicao[1]+5, 2, fill=True)
-#           content[j][i] = content[j][i].split("\n")
-#           print(len(content[j][i]))
-#           for k in range(len(content[j][i])):
-#               cnv.setFillColorRGB(1, 1, 1)
-#               cnv.setFont("Helvetica", 11)
-#               cnv.drawString(20, posicao[1], content[j][i][k])
-#               posicao[1] = posicao[1] - 15
-#   posicao[1] = posicao[1] - 50
-        
-
-def Exibir_Ponto(content, posicao):
+def Exibir_Ponto(content, posicao, tamanho):
     
     for j in range(len(content)):
         for i in range(len(content[j])):
             posicao[1] = posicao[1]-15
             cnv.setFillColorRGB(0, 0, 0)
             if i == 0:
-                cnv.setFont("Helvetica-Bold", 16)
+                cnv.setFont("Helvetica-Bold", tamanho+5)
                 cnv.drawString(posicao[0], posicao[1], content[j][i])
                 cnv.setStrokeColorRGB(1, 1, 1)
                 cnv.line(posicao[0], posicao[1]-5,posicao[0]+200, posicao[1]-5)
@@ -83,10 +73,10 @@ def Exibir_Ponto(content, posicao):
                 print(len(content[j][i]))
                 for k in range(len(content[j][i])):
                     cnv.setFillColorRGB(0, 0, 0)
-                    cnv.setFont("Helvetica", 11)
+                    cnv.setFont("Helvetica", tamanho)
                     cnv.drawString(posicao[0], posicao[1], content[j][i][k])
                     posicao[1] = posicao[1] - 15
-        posicao[1] = posicao[1] - 5
+        posicao[1] = posicao[1] - 30
         
 
 
@@ -97,44 +87,51 @@ def Exibir_Sem(content, posicao):
             posicao[1] = posicao[1]-25
             cnv.setFillColorRGB(0, 0, 0)
             if i == 0:
-                cnv.setFont("Helvetica-Bold", 12)
+                cnv.setFont("Helvetica-Bold", 16)
                 cnv.drawString(posicao[0], posicao[1], content[j][i])
+                cnv.setStrokeColorRGB(1, 1, 1)
+                cnv.line(posicao[0], posicao[1]-5,posicao[0]+200, posicao[1]-5)
+                #posicao[1] = posicao[1]-15
             else:
                 content[j][i] = content[j][i].split("\n")
                 print(len(content[j][i]))
                 for k in range(len(content[j][i])):
                     cnv.setFillColorRGB(0, 0, 0)
-                    cnv.setFont("Helvetica", 11)
+                    cnv.setFont("Helvetica", 12)
                     cnv.drawString(posicao[0], posicao[1], content[j][i][k])
                     posicao[1] = posicao[1] - 15
         posicao[1] = posicao[1] - 30
-        cnv.setStrokeColorRGB(1, 1, 1)
-        cnv.line(posicao[0], posicao[1],posicao[0]+200, posicao[1] )
+        #cnv.setStrokeColorRGB(1, 1, 1)
+        #cnv.line(posicao[0], posicao[1],posicao[0]+200, posicao[1] )
 
-
+#Nome
 splitar = Name.split("\n")
-for i in range(len(splitar)):
-    cnv.setFillColorRGB(i, i, i)
-    cnv.setFont("Helvetica-Bold", 20)
-    cnv.drawString(posicao0[0],posicao0[1]-15-20*i, splitar[i])
-cnv.setFillColorRGB(1, 1, 1)
-cnv.roundRect(posicao0[0]-5,posicao0[1]-55, 230, 15, 10, stroke=0, fill=1)
+cnv.setFont("Helvetica-Bold", 25)
 cnv.setFillColorRGB(0, 0, 0)
-cnv.setFont("Helvetica-Bold", 11)
-cnv.drawString(posicao0[0],posicao0[1]-50, "Sétimo período de Engenharia Biomédica")
+cnv.drawString(posicao0[0]+260,posicao0[1]-50, splitar[0])
+cnv.setFillColorRGB(115/255, 69/255, 85/255)
+cnv.drawString(posicao0[0]+260,posicao0[1]-80, splitar[1])
 
+#Declaração
+cnv.setFont("Helvetica-Bold", 10)
+cnv.setFillColorRGB(0, 0, 0)
+cnv.drawString(posicao0[0]+240,posicao0[1]-815, "*Todo esse documento Foi gerado por mim em um código python")
 
+#Exibir conteúdo
+Exibir_Ponto(content1, posicao1, 11)
+Exibir_Sem(content2, posicao1)
+Exibir_Ponto(content3, posicao4_copia, 14)
+cnv.setFillColorRGB(0.9, 0.9, 0.9)
+cnv.circle(posicao0[0]+30, posicao0[1]-30,75, fill = 1, stroke = 0)
+cnv.drawImage(fr"C:\Users\Julio\Desktop\CoisasCesar\Minhas fotos\OIG.png", posicao0[0]+30, posicao0[1]-170,150, 150, mask = 'auto')
 
-Exibir_Ponto(content1, posicao1)
-Exibir_Sem(content2, posicao3)
-#Exibir(contact_info, posicao3)
-
+#Contatos
 for i in range(len(contact_info[0])):
     print(i)
     cnv.setFillColorRGB(0, 0, 0)
     cnv.setFont("Helvetica-Bold", 12)
-    cnv.drawString(posicao0[0]+350, posicao0[1]-723-20*i, contact_info[0][i])
-    cnv.drawImage(rf"C:\Users\Julio\Desktop\Programacao\Currículo\icones\img{i}.png",posicao0[0]+325, posicao0[1]-730-19*i, 15, 15, mask = 'auto')
+    cnv.drawString(posicao4[0], posicao1[1]-180-20*i, contact_info[0][i])
+    cnv.drawImage(rf"C:\Users\Julio\Desktop\Programacao\Currículo\icones\img{i}.png",posicao4[0]-25, posicao1[1]-185-20*i, 15, 15, mask = 'auto')
 
 
 
